6da06b577b70e4513604d3e5a623a38b1b1fb9cc
Andreas Traut <andreas.traut@gmx.de>
2020-11-19 17:09:51 +0100

    Update Marathon example app

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-# Visualize-Results-in-Apps 
 
-Work in progress...
+
+Author: Andreas Traut  
+Date: 21.01.2020     
+
+# Visualize results in data apps 
+
+Data Scientist often forget, that all their visualizations (and also model), which they have built, need to be used by someone, who is probably not as skilled in all these technical requirements. Therefore it is important to find a solution, which is easy to use for everyone, stable and quickly customizable. There are different solutions, but I found [Streamlit](https://www.streamlit.io/) fantastic, because I didn't have to spend time on building a webpage or learn HTML, CSS or Wordpress. Everyhing is in Python and once the setup is done (which is easy) everything I have to do for updating the whole data app is to save the Python file (no compiling needed). 
+
+I used the data of the "Maraton runtimes" example above and as you can see I only had to change some very minor things in the python code (like `import stramlit as st` and write `st.pyplot(g)` instead of `plt.show()`) in order to create a "data app". You can upload another Excel-csv file by pressing the "Browse files" button, which will then be visualized. Using the checkboxes below will open more graphics (like histograms, kernel density, violion plots,...). See my [data app here](https://share.streamlit.io/andreastraut/visualize-results-in-apps/main/app_Example_Marathon_extended.py) and play around yourself. 
+
+
+
+
+
+# MIT License
+
+Copyright (c) 2020 Andras Traut
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- app_Example_Marathon_extended.py
+++ app_Example_Marathon_extended.py
@@ -12,6 +12,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 st.title("Example Marathon")
+st.write("Author: Andreas Traut, Date: 19.11.2020")
 
 #%% 1 Read the data. The function "convert" will split the Data after ":"
 def convert(s):
@@ -20,6 +21,12 @@ def convert(s):
 
 data=pd.read_csv('marathon-data_extended.csv', converters={'split':convert, 'final':convert})
 # print(data.dtypes)
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # bytes_data = uploaded_file.read()    
+    data=pd.read_csv(uploaded_file, converters={'split':convert, 'final':convert})
+
 
 #%% 2 Apply the converter "convert" to transform the hh:mm:ss. 
 data['split_sec'] = data['split'] / np.timedelta64(1, 's') #EBook PDF page 351: data['split_sec'] = data['split'].astype(int) / 1E9  does not work
@@ -39,55 +46,65 @@ with sns.axes_style('white'):
 st.pyplot(g)
 
 #%% 5 Histogram for 'size' and 'weight' (distplot=Distribution Plot) 
-g = sns.displot(data['size'], kde=False);
-st.pyplot(g)
-# plt.show()
-g = sns.displot(data['weight'], kde=False)
-st.pyplot(g)
+if st.checkbox('Show Histograms'):
+    g = sns.displot(data['size'], kde=False);
+    st.pyplot(g)
+    # plt.show()
+    g = sns.displot(data['weight'], kde=False)
+    st.pyplot(g)
 
 #%% 6 PairGrids with variables 'nationality', 'size', 'final_sec', 'weight'
 #   colors for gender (hue) is GreenBlue (GnBu)
-g = sns.PairGrid(data, vars=['nationality', 'size', 'final_sec', 'weight'],
-                  hue='gender', palette='GnBu_r')
-g.map(plt.scatter, alpha=0.8)
-g.add_legend();
-st.pyplot(g)
+if st.checkbox('Show Pair Grids'):
+    g = sns.PairGrid(data, vars=['nationality', 'size', 'final_sec', 'weight'],
+                     hue='gender', palette='GnBu_r')
+    g.map(plt.scatter, alpha=0.8)
+    g.add_legend();
+    st.pyplot(g)
 
 #%% 7 KernelDensity (kde) for column "size_to_weight" 
-sns.kdeplot(data.size_to_weight[data.nationality=='DE'], label='Deutschland', shade=True)
-sns.kdeplot(data.size_to_weight[data.nationality=='AU'], label='Österreich', shade=True)
-plt.xlabel('size_to_weight');
-# plt.show()
-st.pyplot(plt)
+if st.checkbox('Show Size-to-Weights Kernel Density'):
+    fig, ax = plt.subplots() 
+    ax1 = sns.kdeplot(data.size_to_weight[data.nationality=='DE'], label='Deutschland', shade=True)
+    ax2 = sns.kdeplot(data.size_to_weight[data.nationality=='AU'], label='Österreich', shade=True)
+    plt.xlabel('size_to_weight');
+    # plt.show()
+    st.pyplot(fig)
 
 #%% 8 KernelDensity (kde) for column "weight" 
-sns.kdeplot(data.weight[data.nationality=='DE'], label='Deutschland', shade=True)
-sns.kdeplot(data.weight[data.nationality=='AU'], label='Österreich', shade=True)
-plt.xlabel('size');
-st.pyplot(plt)
+if st.checkbox('Show German/Austria Kernel Density'):
+    fig, ax = plt.subplots() 
+    ax1 = sns.kdeplot(data.weight[data.nationality=='DE'], label='Deutschland', shade=True)
+    ax2 = sns.kdeplot(data.weight[data.nationality=='AU'], label='Österreich', shade=True)
+    plt.xlabel('size');
+    # plt.show()
+    st.pyplot(fig)
 
 #%% 9 Regression Plot for "weight" and "size"
-h = sns.lmplot('weight', 'size', hue='nationality', data=data[data.gender=="M"], markers=".")              
-h = sns.lmplot('weight', 'size', hue='nationality', data=data[data.gender=="W"], markers=".")              
-st.pyplot(h)
-
+if st.checkbox('Show Weight-Size Regression Plots'):
+    m=sns.lmplot('weight', 'size', hue='nationality', data=data[data.gender=="M"], markers=".")              
+    st.pyplot(m)
+    w=sns.lmplot('weight', 'size', hue='nationality', data=data[data.gender=="W"], markers=".")              
+    st.pyplot(w)
+    
 #%% 10 Violinplot using "size" and "nationality"
 men = (data.gender == 'M')
 women = (data.gender == 'W')
-fig, ax = plt.subplots() 
-with sns.axes_style(style=None):
-    ax=sns.violinplot("size", "nationality", hue="gender", data=data,
-                    split=True, inner="quartile",
-                    palette=["lightblue", "lightpink"]);
-# plt.show()
-st.pyplot(fig)
+if st.checkbox('Show Size-Nationality Violinplot'):
+    fig, ax = plt.subplots() 
+    with sns.axes_style(style=None):
+        ax=sns.violinplot("size", "nationality", hue="gender", data=data,
+                          split=True, inner="quartile",
+                          palette=["lightblue", "lightpink"]);
+        # plt.show()
+    st.pyplot(fig)
 
 #%% 11 Violinplot using "weight" and "nationality"
-
-fig, ax = plt.subplots() 
-with sns.axes_style(style=None):
-    ax=sns.violinplot("weight", "nationality", hue="gender", data=data,
-                    split=True, inner="quartile",
-                    palette=["lightblue", "lightpink"]);
-st.pyplot(fig)
+if st.checkbox('Show Weight-Nationality Violinplot'):
+    fig, ax = plt.subplots() 
+    with sns.axes_style(style=None):
+        ax=sns.violinplot("weight", "nationality", hue="gender", data=data,
+                          split=True, inner="quartile",
+                          palette=["lightblue", "lightpink"]);
+    st.pyplot(fig)
 
